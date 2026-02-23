85d9f6253848abe1bf8a8462b6d2aea5aaf5301a
Gerber Rogelio Díaz Santay <rogerdisan18@gmail.com>
2026-02-22 20:12:10 -0600

    Menú y entradas implementadas

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿Console.WriteLine("Sistema de Triaje Médico");
+Console.WriteLine("Tipo de Atención:");
+Console.WriteLine("1. Emergencia");
+Console.WriteLine("2. Consulta");
+Console.Write("3 Pediatría");
+Console.Write("Traumatología");
+int tipo = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la edad del paciente:");
+int edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Temperatura (°C):");
+double temperatura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Saturación de oxígeno (%)");
+int saturacion = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Nivel de dolor (0-10):");
+int dolor = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Presión sistólica");
+int presionSistolica = int.Parse(Console.ReadLine());
