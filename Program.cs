9edd3121286d89104f22ae66921f0f40dae17f55
Gerber Rogelio Díaz Santay <rogerdisan18@gmail.com>
2026-02-22 21:19:32 -0600

    Logica completa yippie

--- Program.cs
+++ Program.cs
@@ -54,5 +54,83 @@ switch (tipo)
             Console.WriteLine("Prioridad 3 - Consulta normal");
         }
         break;
+
+ case 2: // Consulta
+    Console.Write("Temperatura: ");
+    double temp2 = double.Parse(Console.ReadLine());
+    if (temp2 < 35 || temp2 > 42)
+    {
+        Console.WriteLine("Temperatura no valida");
+        break;
+    }
+
+    Console.Write("Dolor (0-10): ");
+    int dolor2 = int.Parse(Console.ReadLine());
+    if (dolor2 < 0 || dolor2 > 10)
+    {
+        Console.WriteLine("Dolor no valido");
+        break;
+    }
+
+    if (temp2 >= 38.5 || dolor2 >= 7)
+    {
+        Console.WriteLine("Prioridad 3 - Consulta prioritaria");
+    }
+    else
+    {
+        Console.WriteLine("Prioridad 4 - Consulta normal");
+    }
+    break;
+
+case 3: // Pediatria
+    Console.Write("Edad: ");
+    int edad3 = int.Parse(Console.ReadLine());
+    if (edad3 < 0 || edad3 > 150)
+    {
+        Console.WriteLine("Edad no valida");
+        break;
+    }
+
+    Console.Write("Temperatura: ");
+    double temp3 = double.Parse(Console.ReadLine());
+    if (temp3 < 35 || temp3 > 42)
+    {
+        Console.WriteLine("Temperatura no valida");
+        break;
+    }
+
+    if (edad3 < 12 && temp3 >= 38.5)
+    {
+        Console.WriteLine("Prioridad 2 - Atencion pediatrica urgente");
+    }
+    else
+    {
+        Console.WriteLine("Prioridad 3 - Consulta pediatrica");
+    }
+    break;
+
+case 4: // Traumatologia
+    Console.Write("Dolor (0-10): ");
+    int dolor4 = int.Parse(Console.ReadLine());
+    if (dolor4 < 0 || dolor4 > 10)
+    {
+        Console.WriteLine("Dolor no valido");
+        break;
+    }
+
+    if (dolor4 >= 8)
+    {
+        Console.WriteLine("Prioridad 2 - Atencion traumatologica urgente");
+    }
+    else
+    {
+        Console.WriteLine("Prioridad 3 - Consulta traumatologica");
+    }
+    break;
+
+default:
+    Console.WriteLine("Opcion no valida");
+    break;
 }
+
 
