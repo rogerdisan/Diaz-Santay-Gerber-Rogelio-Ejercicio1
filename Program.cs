20145e7c5bd201ff7d17ca9c546ccbd9b69a77a3
Gerber Rogelio Díaz Santay <rogerdisan18@gmail.com>
2026-02-22 21:02:22 -0600

    Menu y entradas

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.WriteLine("=== Triaje Medico ===");
+Console.WriteLine("1. Emergencia");
+Console.WriteLine("2. Consulta");
+Console.WriteLine("3. Pediatria");
+Console.WriteLine("4. Traumatologia");
+Console.Write("Elija una opcion: ");
+
+int tipo = int.Parse(Console.ReadLine());
